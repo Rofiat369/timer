d0f679878603e6c991bb08ecae31d3389fb4a0c0
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2021-11-30 13:04:37 +0000

    created an alarm using settimeout function

---
+++ timer1.js
@@ -0,0 +1,24 @@
+const times = process.argv.slice(2);
+
+const timerAlert = function(times) {
+  for (const time of times) {
+    if(time < 0) {
+      continue;
+    }
+
+    if(isNaN(time)) {
+      continue;
+    }
+
+    if(typeof time === 'undefined') {
+      continue;
+    }
+
+    setTimeout(() => {
+      process.stdout.write("\x07");
+     }, time * 1000);
+  }
+};
+
+
+timerAlert(times);
